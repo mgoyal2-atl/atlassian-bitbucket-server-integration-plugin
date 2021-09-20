688e8b17e1bd9468fa62e66bbd0619c6d77f7113
Kristy Hughes <khughes@atlassian.com>
2021-09-20 18:23:01 +1000

    Add BitbucketSCMRepositoryEnvironmentContributor

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMEnvironmentContributor.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMEnvironmentContributor.java
@@ -1,86 +1,23 @@
 package com.atlassian.bitbucket.jenkins.internal.scm;
 
-import com.google.common.annotations.VisibleForTesting;
-import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.EnvVars;
 import hudson.Extension;
-import hudson.model.*;
-import hudson.plugins.git.GitSCM;
-import hudson.scm.SCM;
-import jenkins.branch.BranchSource;
-import jenkins.branch.MultiBranchProject;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import hudson.model.EnvironmentContributor;
+import hudson.model.Run;
+import hudson.model.TaskListener;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
-import java.util.Objects;
 
 @Extension
 public class BitbucketSCMEnvironmentContributor extends EnvironmentContributor {
 
-    public void buildEnvironmentFor(@NonNull Job job, @NonNull EnvVars envs, @NonNull TaskListener listener) throws IOException, InterruptedException {
-        if (job instanceof AbstractProject) {
-            // Case 1 - Freestyle job
-            SCM scm = ((AbstractProject<?, ?>) job).getScm();
-            if (!(scm instanceof BitbucketSCM)) {
-                return;
-            }
-            ((BitbucketSCM) scm).getBitbucketSCMRepository().buildEnvironment(envs);
+    public void buildEnvironmentFor(@Nonnull Run run, @Nonnull EnvVars envs, @Nonnull TaskListener listener) throws IOException, InterruptedException {
+        BitbucketSCMRevisionAction revisionAction = run.getAction(BitbucketSCMRevisionAction.class);
+        if (revisionAction == null) {
+            // No environment to contribute yet
             return;
         }
-        if (job instanceof WorkflowJob) {
-            // Case 2 - Not a freestyle job. Proceed to inspect SCM on item
-            WorkflowJob workflowJob = (WorkflowJob) job;
-            SCM scm = workflowJob.getTypicalSCM();
-            if (!(scm instanceof GitSCM || scm instanceof BitbucketSCM)) {
-                return;
-            }
-            if (scm instanceof BitbucketSCM) {
-                //case 2 - bb_checkout step in the script (pipeline or groovy)
-                ((BitbucketSCM) scm).getBitbucketSCMRepository().buildEnvironment(envs);
-                return;
-            }
-            ItemGroup<?> parent = getJobParent(job);
-            GitSCM gitScm = (GitSCM) scm;
-            if (parent instanceof MultiBranchProject) { // Case 3.1 - Multi branch workflow job
-                MultiBranchProject<?, ?> multiBranchProject = (MultiBranchProject<?, ?>) parent;
-                multiBranchProject
-                        .getSources()
-                        .stream()
-                        .map(BranchSource::getSource)
-                        .filter(BitbucketSCMSource.class::isInstance)
-                        .map(BitbucketSCMSource.class::cast)
-                        .filter(bbsSource ->
-                                // The assumption is the remote URL specified in GitSCM should be same as remote URL
-                                // specified in Bitbucket Source.
-                                gitScm.getUserRemoteConfigs()
-                                        .stream()
-                                        .anyMatch(userRemoteConfig ->
-                                                Objects.equals(userRemoteConfig.getUrl(), bbsSource.getRemote())))
-                        .findFirst()
-                        .ifPresent(scmSource -> scmSource.getBitbucketSCMRepository().buildEnvironment(envs));
-            } else { // Case 3.2 - Part of pipeline run
-                // Handle only SCM jobs.
-                workflowJob.getSCMs()
-                        .stream()
-                        .filter(BitbucketSCM.class::isInstance)
-                        .map(BitbucketSCM.class::cast)
-                        .filter(bScm -> {
-                            GitSCM bGitScm = bScm.getGitSCM();
-                            return bGitScm != null &&
-                                    Objects.equals(bGitScm.getKey(), scm.getKey());
-                        })
-                        .findFirst()
-                        .ifPresent(bScm -> bScm.getBitbucketSCMRepository().buildEnvironment(envs));
-            }
-        }
-    }
-
-    /**
-     * {@link Job#getParent()} uses {@code @WithBridgeMethods(value=Jenkins.class,castRequired=true)} which is
-     * problematic when trying to mock the parent.
-     */
-    @VisibleForTesting
-    ItemGroup<?> getJobParent(Job<?, ?> job) {
-        return job.getParent();
+        revisionAction.getBitbucketSCMRepo().buildEnvironment(envs);
     }
 }
--- src/test/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMEnvironmentContributorTest.java
+++ src/test/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMEnvironmentContributorTest.java
@@ -1,217 +1,31 @@
 package com.atlassian.bitbucket.jenkins.internal.scm;
 
 import hudson.EnvVars;
-import hudson.model.FreeStyleProject;
-import hudson.plugins.git.GitSCM;
-import hudson.plugins.git.UserRemoteConfig;
-import hudson.scm.SCM;
-import jenkins.branch.BranchSource;
-import jenkins.branch.MultiBranchProject;
-import jenkins.scm.api.SCMSource;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import hudson.model.Run;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import javax.annotation.CheckForNull;
-import java.util.UUID;
-
-import static java.util.Collections.singletonList;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
 public class BitbucketSCMEnvironmentContributorTest {
 
-    private static final String REMOTE_URL = "http://localhost:7990/bitbucket/scm/project_1/rep_1.git";
-
-    @Mock
-    BitbucketSCMRepository repository;
-
     @Spy
     BitbucketSCMEnvironmentContributor contributor;
 
     @Test
-    public void testBuildEnvironmentForAbstractProjectWithoutBitbucketSCM() throws Exception {
-        FreeStyleProject job = mock(FreeStyleProject.class);
-        GitSCM scm = mockGitSCM();
-        when(job.getScm()).thenReturn(scm);
-        EnvVars envs = new EnvVars();
+    public void testBuildEnvironment() throws Exception {
+        Run run = mock(Run.class);
+        BitbucketSCMRevisionAction action = mock(BitbucketSCMRevisionAction.class);
+        when(run.getAction(BitbucketSCMRevisionAction.class)).thenReturn(action);
+        BitbucketSCMRepository repo = mock(BitbucketSCMRepository.class);
+        when(action.getBitbucketSCMRepo()).thenReturn(repo);
+        EnvVars env = new EnvVars();
 
-        contributor.buildEnvironmentFor(job, envs, null);
+        contributor.buildEnvironmentFor(run, env, null);
 
-        verify(repository, never()).buildEnvironment(envs);
-    }
-
-    @Test
-    public void testBuildEnvironmentForAbstractProjectWithBitbucketSCM() throws Exception {
-        FreeStyleProject job = mock(FreeStyleProject.class);
-        BitbucketSCM scm = mockBitbucketSCM();
-        when(job.getScm()).thenReturn(scm);
-        EnvVars envs = new EnvVars();
-
-        contributor.buildEnvironmentFor(job, envs, null);
-
-        verify(repository).buildEnvironment(envs);
-    }
-
-    @Test
-    public void testBuildEnvironmentForWorkflowJobWithoutSCM() throws Exception {
-        WorkflowJob job = mockWorkflowJob(null);
-        EnvVars envs = new EnvVars();
-
-        contributor.buildEnvironmentFor(job, envs, null);
-
-        verify(repository, never()).buildEnvironment(envs);
-    }
-
-    @Test
-    public void testBuildEnvironmentForWorkflowJobWithDifferentSCM() throws Exception {
-        SCM scm = mock(SCM.class);
-        WorkflowJob job = mockWorkflowJob(scm);
-        EnvVars envs = new EnvVars();
-
-        contributor.buildEnvironmentFor(job, envs, null);
-
-        verify(repository, never()).buildEnvironment(envs);
-    }
-
-    @Test
-    public void testBuildEnvironmentForWorkflowJobWithBitbucketSCM() throws Exception {
-        BitbucketSCM bitbucketSCM = mockBitbucketSCM();
-        WorkflowJob job = mockWorkflowJob(bitbucketSCM);
-        EnvVars envs = new EnvVars();
-
-        contributor.buildEnvironmentFor(job, envs, null);
-
-        verify(repository).buildEnvironment(envs);
-    }
-
-    @Test
-    public void testBuildEnvironmentForWorkflowJobWithMultibranchParent() throws Exception {
-        GitSCM gitSCM = mockGitSCM();
-        WorkflowJob job = mockWorkflowJob(gitSCM);
-        BitbucketSCMSource bitbucketSCMSource = getBitbucketSCMSource();
-        MultiBranchProject parent = mock(MultiBranchProject.class);
-        when(parent.getSources()).thenReturn(singletonList(new BranchSource(bitbucketSCMSource)));
-        doReturn(parent).when(contributor).getJobParent(job);
-        EnvVars envs = new EnvVars();
-
-        contributor.buildEnvironmentFor(job, envs, null);
-
-        verify(repository).buildEnvironment(envs);
-    }
-
-    @Test
-    public void testBuildEnvironmentForWorkflowJobWithMultibranchParentButNotBitbucketSCMSource() throws Exception {
-        GitSCM gitSCM = mockGitSCM();
-        WorkflowJob job = mockWorkflowJob(gitSCM);
-        MultiBranchProject parent = mock(MultiBranchProject.class);
-        SCMSource notBitbucketScmSource = mock(SCMSource.class);
-        when(parent.getSources()).thenReturn(singletonList(new BranchSource(notBitbucketScmSource)));
-        doReturn(parent).when(contributor).getJobParent(job);
-        EnvVars envs = new EnvVars();
-
-        contributor.buildEnvironmentFor(job, envs, null);
-
-        verify(repository, never()).buildEnvironment(envs);
-    }
-
-    @Test
-    public void testBuildEnvironmentForWorkflowJobWithMultibranchParentButRemotesDontMatch() throws Exception {
-        GitSCM gitSCM = mockGitSCM();
-        WorkflowJob job = mockWorkflowJob(gitSCM);
-        BitbucketSCMSource bitbucketSCMSource = getBitbucketSCMSource();
-        when(bitbucketSCMSource.getRemote()).thenReturn("something else");
-        MultiBranchProject parent = mock(MultiBranchProject.class);
-        when(parent.getSources()).thenReturn(singletonList(new BranchSource(bitbucketSCMSource)));
-        doReturn(parent).when(contributor).getJobParent(job);
-        EnvVars envs = new EnvVars();
-
-        contributor.buildEnvironmentFor(job, envs, null);
-
-        verify(repository, never()).buildEnvironment(envs);
-    }
-
-    @Test
-    public void testBuildEnvironmentForPipelineJobWithNotBitbucketSCM() throws Exception {
-        GitSCM gitSCM = mockGitSCM();
-        WorkflowJob job = mockWorkflowJob(gitSCM);
-        doReturn(null).when(contributor).getJobParent(job);
-
-        EnvVars envs = new EnvVars();
-
-        contributor.buildEnvironmentFor(job, envs, null);
-
-        verify(repository, never()).buildEnvironment(envs);
-    }
-
-    @Test
-    public void testBuildEnvironmentForPipelineJobWithDifferentSCM() throws Exception {
-        GitSCM gitSCM = mockGitSCM();
-        WorkflowJob job = mockWorkflowJob(gitSCM);
-        BitbucketSCM bitbucketSCM = mockBitbucketSCM(); // Will generate a different git scm
-        doReturn(singletonList(bitbucketSCM)).when(job).getSCMs();
-        doReturn(null).when(contributor).getJobParent(job);
-
-        EnvVars envs = new EnvVars();
-
-        contributor.buildEnvironmentFor(job, envs, null);
-
-        verify(repository, never()).buildEnvironment(envs);
-    }
-
-    @Test
-    public void testBuildEnvironmentForPipelineJob() throws Exception {
-        GitSCM gitSCM = mockGitSCM();
-        WorkflowJob job = mockWorkflowJob(gitSCM);
-        BitbucketSCM bitbucketSCM = mockBitbucketSCM(gitSCM);
-        doReturn(singletonList(bitbucketSCM)).when(job).getSCMs();
-        doReturn(null).when(contributor).getJobParent(job);
-
-        EnvVars envs = new EnvVars();
-
-        contributor.buildEnvironmentFor(job, envs, null);
-
-        verify(repository).buildEnvironment(envs);
-    }
-
-    private BitbucketSCMSource getBitbucketSCMSource() {
-        BitbucketSCMSource bitbucketSCMSource = mock(BitbucketSCMSource.class);
-        when(bitbucketSCMSource.getRemote()).thenReturn(REMOTE_URL);
-        when(bitbucketSCMSource.getBitbucketSCMRepository()).thenReturn(repository);
-        return bitbucketSCMSource;
-    }
-
-    private WorkflowJob mockWorkflowJob(@CheckForNull SCM scm) {
-        WorkflowJob job = mock(WorkflowJob.class);
-        when(job.getTypicalSCM()).thenReturn(scm);
-        doReturn(singletonList(scm)).when(job).getSCMs();
-        return job;
-    }
-
-    private GitSCM mockGitSCM() {
-        return mockGitSCM(UUID.randomUUID().toString());
-    }
-
-    private GitSCM mockGitSCM(String gitScmKey) {
-        GitSCM gitSCM = mock(GitSCM.class);
-        UserRemoteConfig gitRemoteConfig = new UserRemoteConfig(REMOTE_URL, null, null, null);
-        when(gitSCM.getUserRemoteConfigs()).thenReturn(singletonList(gitRemoteConfig));
-        when(gitSCM.getKey()).thenReturn(gitScmKey);
-        return gitSCM;
-    }
-
-    private BitbucketSCM mockBitbucketSCM() {
-        GitSCM gitSCM = mockGitSCM();
-        return mockBitbucketSCM(gitSCM);
-    }
-
-    private BitbucketSCM mockBitbucketSCM(GitSCM gitSCM) {
-        BitbucketSCM bitbucketSCM = mock(BitbucketSCM.class);
-        when(bitbucketSCM.getBitbucketSCMRepository()).thenReturn(repository);
-        when(bitbucketSCM.getGitSCM()).thenReturn(gitSCM);
-        return bitbucketSCM;
+        verify(repo).buildEnvironment(env);
     }
 }
